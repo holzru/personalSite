1193697ab88090614925160a6e303d04ef456cb1
Russell Holz <holzru@gmail.com>
2020-05-20 22:32:17 -0700

    Got a job. Under construction until I come up with an orginal design I can bare to look at, lol.

--- src/App.js
+++ src/App.js
@@ -13,22 +13,48 @@ import {
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/resume">
-          <Resume />
-        </Route>
-        <Route path="/">
-          <Redirect to="/projects" />
-        </Route>
-      </Switch>
-    </Router>
+    // <Router>
+    //   <Switch>
+    //     <Route path="/projects">
+    //       <Projects />
+    //     </Route>
+    //     <Route path="/about">
+    //       <About />
+    //     </Route>
+    //     <Route path="/resume">
+    //       <Resume />
+    //     </Route>
+    //     <Route path="/">
+    //       <Redirect to="/projects" />
+    //     </Route>
+    //   </Switch>
+    // </Router>
+    <React.Fragment>
+      <div>
+        <img
+          src="/imgs/construct1.jpg"
+          className="w-screen"
+        />
+      </div>
+      <div className="w-screen">
+        <img
+          src="/imgs/construct2.jpg"
+          className="w-screen"
+        />
+      </div>
+      <div className="w-screen">
+        <img
+          src="/imgs/construct3.jpeg"
+          className="w-screen"
+        />
+      </div>
+      <div className="w-screen">
+        <img
+          src="/imgs/construct4.png"
+          className="w-screen"
+        />
+      </div>
+    </React.Fragment>
   );
 };
 
